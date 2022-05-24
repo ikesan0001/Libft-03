f14a4bd4b28f68b1d7dabd1910ee1c3af8e2671c
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-24 15:26:39 +0900

    strncmp null of both

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 18:55:11 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/24 14:22:38 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/24 15:24:22 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*S1;
 	unsigned char	*S2;
 	size_t			i;
-	
+
+	if (s1 == s2 || n == 0)
+		return (0);
 	i = 0;
 	S1 = (unsigned char *)s1;
 	S2 = (unsigned char *)s2;
-	while(S1[i] != '\0' && S2[i] != '\0' && S1[i] == S2[i] && i < n)
+	while (S1[i] != '\0' && S2[i] != '\0' && S1[i] == S2[i] && i < n)
 		i++;
 	if (i == n)
 		return (0);
