b4900d4dcb1c48068d50dad84bf1a6ecc56ac9df
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-24 14:36:37 +0900

    strnstr 0 -> '\0'

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 16:32:41 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/09 20:56:28 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/24 14:35:50 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,16 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	i = 0;
 	j = 0;
 	h = (char *)haystack;
-	if (ft_strlen(needle) == 0)
+	if (ft_strlen(needle) == '\0')
 		return (h);
-	while (h[i] != 0 && i < len)
+	while (h[i] != '\0' && i < len)
 	{
-		while (h[i + j] == needle[j] && needle[j] != 0 && i + j < len)
+		while (h[i + j] == needle[j] && needle[j] != '\0' && i + j < len)
 			j++;
-		if (needle[j] == 0)
+		if (needle[j] == '\0')
 			return (h + i);
 		j = 0;
 		i++;
 	}
-	return (0);
+	return ('\0');
 }
