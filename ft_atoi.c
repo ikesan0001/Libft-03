b2bfacba7a9844b100d4f0c6ac641e12fecd4ae3
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-25 23:33:55 +0900

    propably all OK.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,55 +6,32 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 21:02:13 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/24 23:31:48 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/25 22:42:51 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isspace(int c);
-int	numcmp(const char *dst, const char src);
-
 int	ft_atoi(const char *str)
 {
-	char		*s;
-	long long	i;
-	int			j;
-	int			sign
-	int			digit;
+	char	*s;
+	long	i;
 
 	i = 0;
 	j = 0;
-	sign = 1;
-	digit = 0;
 	s = (char *)str;
-	while (ft_isspace(s[0]))
+	while (s[0] == '\t' || s[0] == '\n' || s[0] == '\v' || \
+s[0] == '\f' || s[0] == '\r' || s[0] == ' ')
 		s++;
-	if (s[0] == '+' || s[0] == '-')
-	{
-		if (s[0] == '-')
-			sign *= -1;
-		s++;
-	}
+	if (s[j] == '+' || s[j] == '-')
+		j++;
 	while (ft_isdigit(s[j]) != 0)
 	{
 		i = i * 10 + (s[j] - "0");
 		j++;
 		digit++;
 	}
-	if (digit > 18)
-	{
-		if (digit > 19)
-			i = -1 - sign;
-
-	i *= sign;
+	if (s[0] == '-')
+		i *= -1;
 	return ((int)i);
 }
-
-int	ft_isspace(int c)
-{
-	return (c == '\t' || c == '\n' || c == '\v' || \
-c == '\f' || c == '\r' || c == ' ');
-}
-
-int numcmp(const char *dst, const char *src);
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 02:01:43 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/23 22:34:06 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/25 22:29:11 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	while (S1[i] == S2[i] && i < n)
 		i++;
-	if (i ==  n)
+	if (i == n)
 		return (0);
 	return (S1[i] - S2[i]);
 }
