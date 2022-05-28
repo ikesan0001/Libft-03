277c4b7c6293786fc1e842864935b28eef31355a
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-28 14:34:30 +0900

    TOO_MANY_LINES :(

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,12 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 21:02:13 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/27 10:16:42 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/28 11:56:24 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int	ft_isloverflow(char *s, int digit);
 int	ft_isspace(int c);
@@ -24,41 +25,42 @@ int	ft_atoi(const char *str)
 
 	i = 0;
 	j = 0;
+	is = 0;
 	s = (char *)str;
 	while (ft_isspace(s[0]))
 		s++;
 	if (s[j] == '+' || s[j] == '-')
 		j++;
 	while (ft_isdigit(s[j]))
-	{
-		i = i * 10 + (long)(s[j] - '0');
-		j++;
-	}
+		i = i * 10 + (long)(s[j++] - '0');
 	if (s[0] == '-')
 		i *= -1;
-	is = ft_isloverflow(s, --j);
-	if (is > 0)
+	if (--j >= 19)
+		is = ft_isloverflow(s, j);
+	if (is == 1)
 		i = 9223372036854775807;
-	if (is < 0)
-		i = -9223372036854775808;
+	if (is == -1)
+	{
+		i = -9223372036854775807;
+		i--;
+	}
 	return ((int)i);
 }
 
 int	ft_isspace(int c)
 {
-	return (s[0] == '\t' || s[0] == '\n' || s[0] == '\v' || \
-s[0] == '\f' || s[0] == '\r' || s[0] == ' ');
+	return (c == '\t' || c == '\n' || c == '\v' || \
+c == '\f' || c == '\r' || c == ' ');
 }
 
 int	ft_isloverflow(char *s, int digit)
 {
-	int		i;
 	int		sign;
-	char	*edge;
+	char	edge[20];
 
 	sign = 1;
-	edge = "9223372036854775807";
-	if (s[0] == '+' || s[0] '-')
+	ft_strlcpy(edge, "9223372036854775807", 20);
+	if (s[0] == '+' || s[0] == '-')
 	{
 		digit--;
 		if (s[0] == '-')
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 17:12:36 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/27 09:06:32 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/27 10:19:47 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strrchr(const char *s, int c)
 	i = ft_strlen(src);
 	while (i > 0 && src[i] != (char)c)
 		i--;
-	if (S[i] == (char)c)
+	if (src[i] == (char)c)
 		return (src + i);
 	return (NULL);
 }
