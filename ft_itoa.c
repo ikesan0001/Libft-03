2f9767b0874c8e2d5187276123bfa453b571f8e5
iryoga <iryoga@Laptop-02.localdomain>
2022-06-11 23:59:41 +0900

    :)

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,17 +6,54 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 13:46:37 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/08 16:02:51 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/10 13:21:04 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <limits.h>
 
 static	size_t ft_nbrlen(int n);
 
 char	*ft_itoa(int n)
 {
-	size_t	i;
+	size_t	digit;
+	int		i;
 	char	*c;
-	
+	int		backup;
+
+	backup = n;
+	i = 0;
+	digit = ft_nbrlen(n);
+	c = (char *)malloc(digit);
+	if (c = NULL)
+		return (NULL);
+	if (n < 0)
+	{
+	}
+	printf("%d\n%zu\n", n ,digit);
+	return (NULL);
+}
+
+static	size_t	ft_nbrlen(int n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n < 0)
+		i++;
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+int	main(void)
+{
+	ft_itoa(INT_MIN);
+	ft_itoa(INT_MAX);
+	return (0);
 }
