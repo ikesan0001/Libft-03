77b61fc01126581efec021c85269e822d4f48bdb
iryoga <iryoga@student.42tokyo.jp>
2022-06-13 01:09:23 +0900

    itoa OK:)

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,38 +6,42 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 13:46:37 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/12 02:50:00 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/13 01:08:46 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <limits.h>
 
-static	size_t ft_nbrlen(int n);
+static size_t	ft_nbrlen(int n);
+static void		ft_strreverse(char *src);
 
 char	*ft_itoa(int n)
 {
-	size_t	digit;
 	int		i;
 	char	*c;
-	int		backup;
+	int		sign;
 
-	backup = n;
 	i = 0;
-	if (n == INT_MIN)
-	{
-		
-	}
-	digit = ft_nbrlen(n);
-	c = (char *)malloc(digit);
-	if (c = NULL)
+	sign = n;
+	c = (char *)ft_calloc(ft_nbrlen(n), sizeof(char));
+	if (c == NULL)
 		return (NULL);
 	if (n < 0)
 	{
+		c[i++] = n % 10 * -1 + '0';
+		n /= -10;
 	}
-	printf("%d\n%zu\n", n ,digit);
-	return (NULL);
+	if (n == 0)
+		c[i] = '0';
+	while (n > 0)
+	{
+		c[i++] = n % 10 + '0';
+		n /= 10;
+	}
+	if (sign < 0)
+		c[i] = '-';
+	ft_strreverse(c);
+	return (c);
 }
 
 static	size_t	ft_nbrlen(int n)
@@ -45,19 +49,30 @@ static	size_t	ft_nbrlen(int n)
 	size_t	i;
 
 	i = 0;
-	if (n < 0)
+	if (n <= 0)
 		i++;
 	while (n != 0)
 	{
 		n /= 10;
 		i++;
 	}
+	i++;
 	return (i);
 }
 
-int	main(void)
+static	void	ft_strreverse(char *src)
 {
-	ft_itoa(INT_MIN);
-	ft_itoa(INT_MAX);
-	return (0);
+	char	buf;
+	size_t	src_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	i = 0;
+	while (src_len > i)
+	{
+		buf = src[i];
+		src[i++] = src[--src_len];
+		src[src_len] = buf;
+	}	
+	return ;
 }
