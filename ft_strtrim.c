f817fea9369a3209c599689d76b2aa243e6dc0cc
iryoga <iryoga@student.42tokyo.jp>
2022-06-15 10:05:50 +0900

    modify strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:53:28 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/14 22:58:11 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/15 10:05:30 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	c_len = ft_strlen(s1);
 	i = 0;
 	j = c_len - 1;
-	while (j > 0 && ft_strchr(set, s1[j]) != NULL)
+	while (j > 0 && ft_strchr(set, s1[j--]) != NULL)
 		i++;
 	c_len -= i;
 	i = 0;
 	j = 0;
-	while (s1[j] != '\0' && ft_strchr(set, s1[j]) != NULL)
+	while (s1[j] != '\0' && ft_strchr(set, s1[j++]) != NULL)
 		i++;
 	if (i >= c_len)
 		return (NULL);
