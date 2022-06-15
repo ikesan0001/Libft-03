5357405b47ce14584cee04f54f22dc2efc21f25e
iryoga <iryoga@student.42tokyo.jp>
2022-06-15 10:54:21 +0900

    ike!!!!

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:53:28 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/15 10:45:27 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/15 10:54:02 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,22 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (s1 == NULL || set == NULL || ft_strlen(s1) == 0)
 		return (NULL);
-	if (ft_strlen(set) == 0)
-		return (s1);
 	c_len = ft_strlen(s1);
 	i = 0;
 	j = c_len - 1;
-	while (j > 0 && ft_strchr(set, s1[j--]) != NULL)
-		i++;
-	c_len -= i;
-	i = 0;
-	j = 0;
-	while (s1[j] != '\0' && ft_strchr(set, s1[j++]) != NULL)
-		i++;
-	if (i >= c_len)
-		return (NULL);
-	c_len -= i;
+	if (ft_strlen(set) != 0)
+	{
+		while (j > 0 && ft_strchr(set, s1[j--]) != NULL)
+			i++;
+		c_len -= i;
+		i = 0;
+		j = 0;
+		while (s1[j] != '\0' && ft_strchr(set, s1[j++]) != NULL)
+			i++;
+		if (i >= c_len)
+			return (NULL);
+		c_len -= i;
+	}
 	c = ft_calloc(++c_len, sizeof(char));
 	if (c == NULL)
 		return (NULL);
