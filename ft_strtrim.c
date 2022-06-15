27d0d957056273b22fc911fbfb0ffee61b573e46
iryoga <iryoga@student.42tokyo.jp>
2022-06-15 10:28:24 +0900

    wakaran

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:53:28 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/15 10:23:34 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/15 10:28:08 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	j;
 	size_t	c_len;
 
-	c_len = ft_strlen(s1);
-	if (c_len == 0)
+	if (s1 == NULL || set == NULL || ft_strlen(s1) == 0)
 		return (NULL);
+	c_len = ft_strlen(s1);
 	i = 0;
 	j = c_len - 1;
 	while (j > 0 && ft_strchr(set, s1[j--]) != NULL)
