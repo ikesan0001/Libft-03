c89b225e27e81e9dc5e6b954f74f9b31c17c6e0d
iryoga <iryoga@student.42tokyo.jp>
2022-06-23 01:51:28 +0900

    kakunin

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,10 +6,11 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:53:28 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/23 01:48:01 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/23 01:51:16 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -23,7 +24,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	if (set == NULL)
 		return (ft_strdup(s1));
-/*
+
 	c_len = ft_strlen(s1);
 	if (c_len == 0)
 		return (ft_strdup(""));
@@ -39,19 +40,46 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (i >= c_len)
 		return (ft_strdup(""));
 	c_len -= i;
-*/
+
 	i = 0;
-	while (s1[i] != '\0' && ft_strchr(set, s1[i]) == NULL)
+	while (s1[i] != '\0' && ft_strchr(set, s1[i]) != NULL)
 		i++;
 	c_len = ft_strlen(s1 + i);
 	if (c_len == 0)
 		return (ft_strdup(""));
 	j = ft_strlen(s1) - 1;
-	while (j > 0 && ft_strchr(set, s1[j--]) == NULL)
+	while (j > 0 && ft_strchr(set, s1[j--]) != NULL)
 		c_len--;
 	c_len++;
 	c = ft_substr(s1, i, c_len);
 	if (c == NULL)
 		return (NULL);
 	return (c);
+}
+*/
+
+#include "libft.h"
+#define NOT_FOUND NULL
+
+char	*ft_strtrim(char const	*s1, char const	*set)
+{
+	char	*trim_s;
+	size_t	size;
+	size_t	i;
+
+	if (s1 == NULL)
+		return (NULL);
+	if (set == NULL)
+		return (ft_strdup(s1));
+	while (*s1 && (ft_strchr(set, *s1) != NOT_FOUND))
+		s1++;
+	i = ft_strlen(s1);
+	if (i == 0)
+		return (ft_strdup(""));
+	i--;
+	while (i > 0 && (ft_strrchr(set, s1[i]) != NOT_FOUND))
+		i--;
+	size = i + 1;
+	trim_s = ft_substr(s1, 0, size);
+	return (trim_s);
 }
