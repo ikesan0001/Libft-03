ec03167ef2f24193a0d08d82610c2fdd9ed09134
iryoga <iryoga@student.42tokyo.jp>
2022-06-23 01:36:09 +0900

    test trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:53:28 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/23 01:27:29 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/23 01:35:59 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (c_len == 0)
 		return (ft_strdup(""));
 	j = c_len - 1;
-	while (j > 0 && ft_strrchr(set, s1[j]) != NULL)
-		j--;
-	c_len -= j;
+	while (j > 0 && ft_strrchr(set, s1[j--]) != NULL)
+		c_len--;
 	c = ft_substr(s1, i, c_len);
 	return (c);
 }
