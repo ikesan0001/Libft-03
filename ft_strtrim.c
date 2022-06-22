2aeda0b34b8754cc44abded1ea4760855c8c1a27
iryoga <iryoga@student.42tokyo.jp>
2022-06-23 00:46:54 +0900

    test

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:53:28 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/15 10:54:02 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/23 00:46:47 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	j;
 	size_t	c_len;
 
-	if (s1 == NULL || set == NULL || ft_strlen(s1) == 0)
+	if (s1 == NULL)
 		return (NULL);
+	if (set == NULL)
+		return (ft_strdup(s1));
 	c_len = ft_strlen(s1);
+	if (c_len == 0)
+		return (ft_strdup(""));
 	i = 0;
 	j = c_len - 1;
 	if (ft_strlen(set) != 0)
@@ -37,9 +41,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 			return (NULL);
 		c_len -= i;
 	}
-	c = ft_calloc(++c_len, sizeof(char));
-	if (c == NULL)
-		return (NULL);
-	ft_strlcpy(c, s1 + i, c_len);
+	c = ft_substr(s1, i + 1, c_len);
 	return (c);
 }
