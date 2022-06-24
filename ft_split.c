192d3081204bf1bb2cd4dbfdac3b54c01a64757c
iryoga <iryoga@student.42tokyo.jp>
2022-06-25 00:42:12 +0900

    test

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,11 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 13:22:26 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/25 00:17:53 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/25 00:41:15 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+/*
 #include "libft.h"
 
 static size_t	ft_split_count(char const *s, char c);
@@ -97,4 +97,81 @@ static char	**ft_free(char **str, size_t end, char *divider)
 	free(str);
 	free(divider);
 	return (NULL);
+}
+*/
+
+#include "libft.h"
+
+static size_t	ft_split_count(char const	*s, char	c)
+{
+	size_t	cnt;
+	size_t	i;
+
+	i = 0;
+	cnt = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (i == 0 || s[i - 1] == c))
+			cnt++;
+		i++;
+	}
+	return (cnt);
+}
+
+static size_t	ft_splitlen(char const *s, char	c)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0' && s[len] != c)
+		len++;
+	return (len);
+}
+
+static char	**ft_free(char	**str, size_t	end)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= end)
+		free(str[i++]);
+	free(str);
+	return (NULL);
+}
+
+char	**ft_do_split(char	**split_str, char const	*s, char	c, size_t	cnt)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	while (i < cnt)
+	{
+		while (*s && *s == c)
+			s++;
+		len = ft_splitlen(s, c);
+		split_str[i] = (char *)malloc((len + 1) * sizeof(char));
+		if (split_str[i] == NULL)
+			return (ft_free(split_str, i));
+		ft_strlcpy(split_str[i++], s, (len + 1));
+		s += len;
+	}
+	split_str[i] = NULL;
+	return (split_str);
+}
+
+char	**ft_split(char const	*s, char	c)
+{
+	size_t	cnt;
+	char	**split_str;
+
+	if (s == NULL)
+		return (NULL);
+	cnt = ft_split_count(s, c);
+	if (cnt == SIZE_MAX)
+		return (NULL);
+	split_str = (char **)malloc((cnt + 1) * sizeof(char *));
+	if (split_str == NULL)
+		return (NULL);
+	return (ft_do_split(split_str, s, c, cnt));
 }
