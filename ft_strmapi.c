fce3992d47431272032acf6c42ae282740402d4c
iryoga <iryoga@student.42tokyo.jp>
2022-06-27 23:51:09 +0900

    create strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 23:44:03 by iryoga            #+#    #+#             */
+/*   Updated: 2022/06/27 23:50:54 by iryoga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+{
+	char	*mapi;
+	size_t	i;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	mapi = ft_substr(s);
+	if (mapi == NULL)
+		return (NULL);
+	i = 0;
+	return (mapi);
+}
