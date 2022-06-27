8eeca1f25b7061531eebf636b6bfadf8ed2a5e9d
iryoga <iryoga@student.42tokyo.jp>
2022-06-27 23:57:27 +0900

    syuusei

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,13 +6,13 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 23:44:03 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/27 23:50:54 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/27 23:57:10 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*mapi;
 	size_t	i;
