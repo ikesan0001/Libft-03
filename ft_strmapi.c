c338729f6fea378ada295b0ca7c5ec1d6cef8f2a
iryoga <iryoga@student.42tokyo.jp>
2022-06-29 13:03:57 +0900

    strmapi OK.

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 23:44:03 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/27 23:59:43 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/28 19:55:01 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char	*mapi;
-	size_t	i;
+	char			*mapi;
+	unsigned int	i;
+	size_t			s_len;
 
 	if (s == NULL || f == NULL)
 		return (NULL);
@@ -23,5 +24,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	if (mapi == NULL)
 		return (NULL);
 	i = 0;
+	s_len = ft_strlen(s);
+	while (i < s_len)
+		mapi[i] = (*f)(i, s[i++]);
 	return (mapi);
 }
