6ef01ccff6538851a44c189abe13c3552aafc3ab
iryoga <iryoga@student.42tokyo.jp>
2022-06-29 13:57:56 +0900

    putendl first commit.

---
+++ ft_putendl_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 13:54:39 by iryoga            #+#    #+#             */
+/*   Updated: 2022/06/29 13:57:31 by iryoga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	size_t	i;
+	size_t	s_len;
+
+	if (s == NULL)
+		return ;
+	i = 0;
+	s_len = ft_strlen(s);
+	while (i < s_len)
+	{
+		write(fd, &s[i], 1);
+		write(fd, '\n', 1);
+		i++;
+	}
+	return ;
+}
