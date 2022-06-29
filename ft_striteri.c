9db6ae58e4009e5974060d7783883859391a5bee
iryoga <iryoga@student.42tokyo.jp>
2022-06-29 13:21:32 +0900

    iteri void

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,13 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 13:07:04 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/29 13:16:49 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/29 13:21:15 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 	size_t			s_len;
@@ -23,5 +23,5 @@ void	*ft_striteri(char *s, void (*f)(unsigned int, char*))
 	s_len = ft_strlen(s);
 	while (i < s_len)
 		(*f)(i, &s[i++]);
-	return (s);
+	return ;
 }
