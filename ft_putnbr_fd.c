f3822b4eae12ad009fd4d66006d64ba9e6f4007b
iryoga <iryoga@student.42tokyo.jp>
2022-06-30 17:32:23 +0900

    test

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:39:01 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/30 17:28:01 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/06/30 17:32:14 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,11 @@ void	ft_putnbr_fd(int n, int fd)
 		write(fd, "-", 1);
 		long_n *= -1;
 	}
+	printf("\n%ld\n", long_n);
 	if (n >= 10)
 		ft_putnbr_fd(long_n / 10, fd);
 	long_n %= 10;
 	ft_putchar_fd(long_n + '0', fd);
-	printf("\n%c", (char)(long_n + '0'));
+	printf("%c", (char)(long_n + '0'));
 	return ;
 }
