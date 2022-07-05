e0657b91ea05efe1095accedb6d20b6c7aa7cd9c
iryoga <iryoga@student.42tokyo.jp>
2022-07-05 10:35:03 +0900

    add NULL check of f.

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 13:07:04 by iryoga            #+#    #+#             */
-/*   Updated: 2022/06/29 13:27:02 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/07/05 10:34:31 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	i;
 	size_t			s_len;
 
-	if (s == NULL)
+	if (s == NULL || f == NULL)
 		return ;
 	i = 0;
 	s_len = ft_strlen(s);
